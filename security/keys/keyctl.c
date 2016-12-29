fcfaf050ab209d6aff06f45b8f0ae21c24534f60
djmax81 <djmax81@gmail.com>
2016-12-29 08:17:35 -0700

    Linux 3.10.95

--- security/keys/keyctl.c
+++ security/keys/keyctl.c
@@ -741,6 +741,25 @@ long keyctl_read_key(key_serial_t keyid, char __user *buffer, size_t buflen)
 		ret = -EACCES;
 		goto error2;
 	}
+<<<<<<<
+=======
+
+	/* the key is probably readable - now try to read it */
+can_read_key:
+	ret = -EOPNOTSUPP;
+	if (key->type->read) {
+		/* Read the data with the semaphore held (since we might sleep)
+		 * to protect against the key being updated or revoked.
+		 */
+		down_read(&key->sem);
+		ret = key_validate(key);
+		if (ret == 0)
+			ret = key->type->read(key, buffer, buflen);
+		up_read(&key->sem);
+	}
+
+error2:
+>>>>>>>
 
 	/* the key is probably readable - now try to read it */
 can_read_key:
