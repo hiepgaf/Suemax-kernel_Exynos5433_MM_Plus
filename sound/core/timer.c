407aa8b878c5473955e4be835a571f9797cf2fa5
djmax81 <djmax81@gmail.com>
2016-12-29 08:20:27 -0700

    Linux 3.10.96

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1850,6 +1850,18 @@ static long snd_timer_user_ioctl(struct file *file, unsigned int cmd,
 	return ret;
 }
 
+static long snd_timer_user_ioctl(struct file *file, unsigned int cmd,
+				 unsigned long arg)
+{
+	struct snd_timer_user *tu = file->private_data;
+	long ret;
+
+	mutex_lock(&tu->ioctl_lock);
+	ret = __snd_timer_user_ioctl(file, cmd, arg);
+	mutex_unlock(&tu->ioctl_lock);
+	return ret;
+}
+
 static int snd_timer_user_fasync(int fd, struct file * file, int on)
 {
 	struct snd_timer_user *tu;
